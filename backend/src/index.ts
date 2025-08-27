4fb02589742fad42fde69bf8159d65dc2b72a9a8
kaboom771 <savateev-05@mail.ru>
2025-08-27 17:52:55 +0300

    add articles endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,42 @@
 import express from 'express'
 
+const articles = [
+  {
+    nick: 'article nick 1',
+    name: 'article 1',
+    description: 'description of article 1',
+  },
+  {
+    nick: 'article nick 2',
+    name: 'article 2',
+    description: 'description of article 2',
+  },
+  {
+    nick: 'article nick 3',
+    name: 'article 3',
+    description: 'description of article 3',
+  },
+  {
+    nick: 'article nick 4',
+    name: 'article 4',
+    description: 'description of article 4',
+  },
+  {
+    nick: 'article nick 5',
+    name: 'article 5',
+    description: 'description of article 5',
+  },
+]
+
 const expressApp = express()
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
 })
+
+expressApp.get('/articles', (req, res) => {
+  res.send(articles)
+})
+
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000')
 })
