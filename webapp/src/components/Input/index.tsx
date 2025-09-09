e0832f1424f7b12dc2ffb25465833dbc67cc91f8
kaboom771 <savateev-05@mail.ru>
2025-09-09 23:59:58 +0300

    Add form validation

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -3,6 +3,7 @@ import { FormikProps } from 'formik'
 
 export const Input = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const value = formik.values[name]
+  const error = formik.errors[name] as string | undefined
   return (
     <div style={{ marginBottom: 10 }}>
       <label htmlFor={name}>{label}</label>
@@ -16,6 +17,7 @@ export const Input = ({ name, label, formik }: { name: string; label: string; fo
         name={name}
         id={name}
       />
+      {error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   )
 }
--- webapp/src/components/TextArea/index.tsx
+++ webapp/src/components/TextArea/index.tsx
@@ -3,6 +3,7 @@ import { FormikProps } from 'formik'
 
 export const TextArea = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const value = formik.values[name]
+  const error = formik.errors[name] as string | undefined
   return (
     <div style={{ marginBottom: 10 }}>
       <label htmlFor={name}>{label}</label>
@@ -15,6 +16,7 @@ export const TextArea = ({ name, label, formik }: { name: string; label: string;
         name={name}
         id={name}
       />
+      {error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   )
 }
