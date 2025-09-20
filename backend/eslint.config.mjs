48bb61d4af2082800cf6ad0675b8c564e36efb67
kaboom771 <savateev-05@mail.ru>
2025-09-20 16:25:58 +0300

    Add env parser to backend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,18 +1,63 @@
 import baseConfig from '../eslint.config.js'
+import pluginN from 'eslint-plugin-n'
+import globals from 'globals'
+import typescriptParser from '@typescript-eslint/parser'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   ...baseConfig,
 
   {
-    files: ['**/*.{ts,js}'],
+    // ✅ Разделяем конфиги для TS и JS
+    files: ['**/*.ts'],
     languageOptions: {
-      parserOptions: {
-        project: './tsconfig.json',
+      //parser: typescriptParser, // Обязательно для TypeScript(надо найти другой парсер)
+      globals: {
+        ...globals.node, // Все Node.js глобалы
       },
+      parserOptions: {
+        project: './tsconfig.json', // Для typed linting
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      n: pluginN,
+    },
+    rules: {
+      'n/no-process-env': [
+        'warn',
+        {
+          allowedVariables: [],
+        },
+      ],
     },
   },
   {
-    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node, // Все Node.js глобалы
+      },
+    },
+    plugins: {
+      n: pluginN,
+    },
+    rules: {
+      'n/no-process-env': [
+        'warn',
+        {
+          allowedVariables: [],
+        },
+      ],
+    },
+  },
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      'coverage',
+      'eslint.config.js',
+      'eslint.config.mjs', // Добавьте это
+    ],
   },
 ]
