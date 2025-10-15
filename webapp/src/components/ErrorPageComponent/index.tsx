9b6f29feca0a308bc6feff565ac6e1899debb962
kaboom771 <savateev-05@mail.ru>
2025-10-16 01:18:32 +0300

    Add 404 image

--- webapp/src/components/ErrorPageComponent/index.tsx
+++ webapp/src/components/ErrorPageComponent/index.tsx
@@ -1,16 +1,20 @@
+import React from 'react'
 import { Alert } from '../Alert'
 import { Segment } from '../segment'
 
 export const ErrorPageComponent = ({
   title = 'Oops, error',
   message = 'Something went wrong',
+  children
 }: {
   title?: string
   message?: string
+  children?: React.ReactNode
 }) => {
   return (
     <Segment title={title}>
       <Alert color="red">{message}</Alert>
+      {children}
     </Segment>
   )
 }
